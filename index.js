82014fb73508e0b1b3bec6d3972f80a8b2d2d3a0
Abdur Rahman Argon <shehab7331@gamil.com>
2022-05-05 14:17:59 +0600

    mongo db  connect and working

--- index.js
+++ index.js
@@ -1,124 +1,46 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-const product = [
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-  {
-    serial: 1,
-    supplierName: "Nasim",
-    productName: "watch",
-    price: "1500",
-    img: "https://i.ibb.co/BCNjjjN/Urine.png",
-    description:
-      "I am a simple person of my bpersomnalotiy this is my book of self",
-    stock: "23",
-    delivery: "12",
-  },
-];
+const uri =
+  "mongodb+srv://databaseassainment11:YtNby235AV4WjbLU@cluster0.3r9sp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
+//databaseassainment11
+//YtNby235AV4WjbLU
+
+async function run() {
+  try {
+    await client.connect();
+    const itemsCollection = client
+      .db("databaseAssainment11")
+      .collection("productItems");
+
+    app.get("/productItems", async (req, res) => {
+      const query = {};
+      const cursor = itemsCollection.find(query);
+      const productItems = await cursor.toArray();
+      res.send(productItems);
+    });
+  } finally {
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("product");
+});
 app.get("/product", (req, res) => {
   res.send(product);
 });
