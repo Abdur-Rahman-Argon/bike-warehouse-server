2c77323390ea39db2a7d0d36c1d6ce6e0024a4b3
Abdur Rahman Argon <shehab7331@gamil.com>
2022-05-06 00:13:33 +0600

    database get method success

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
@@ -8,29 +8,49 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://databaseassainment11:YtNby235AV4WjbLU@cluster0.3r9sp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//mongo url
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3r9sp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+//mongo client
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
-//databaseassainment11
-//YtNby235AV4WjbLU
-
+//server with mongo
 async function run() {
   try {
     await client.connect();
     const itemsCollection = client
       .db("databaseAssainment11")
       .collection("productItems");
-
-    app.get("/productItems", async (req, res) => {
-      const query = {};
-      const cursor = itemsCollection.find(query);
-      const productItems = await cursor.toArray();
+    const query = {};
+    const cursor = itemsCollection.find(query);
+    const productItems = await cursor.toArray();
+    // mongo get data api
+    app.get("/productItems", (req, res) => {
       res.send(productItems);
+    });
+
+    // single data api
+    app.get("/productItem/:id", (req, res) => {
+      const id = req.params.id;
+      const productItem = productItems.find((product) => product._id == id);
+      res.send(productItem);
+    });
+
+    // mongo post data
+    app.post("/productItems", async (req, res) => {
+      const newItem = req.body;
+      const result = await itemsCollection.insertOne(newItem);
+      res.send(result);
+    });
+    // mongo remove data
+    app.delete("/productItem/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await itemsCollection.deleteOne(query);
+      res.send(result);
     });
   } finally {
     //await client.close();
@@ -41,10 +61,8 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("product");
 });
-app.get("/product", (req, res) => {
-  res.send(product);
-});
 
+//port runing
 app.listen(port, () => {
   console.log("server connect port", port);
 });
